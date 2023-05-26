28642e4f7708d9bf76b11a27b18bc450580814f2
keybot <mail.keybot@gmail.com>
2023-05-26 16:14:41 -0300

    fix: a hover cursor pointer css

--- src/app/page.js
+++ src/app/page.js
@@ -81,7 +81,7 @@ export default function Home() {
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Resume{' '}
+            Resumé{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
             -&gt;
           </span>
